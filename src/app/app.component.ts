231d620697311f392330aecf929e985443617e74
khoi <hnak2442001smg4@gmail.com>
2024-05-16 22:05:31 +0700

    Created Header, Sidebar.Creating Courses Page, doing filter function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CoursesComponent } from './courses/courses.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CoursesComponent],
+  imports: [RouterOutlet, CoursesComponent, HeaderComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -2,19 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { CoursesList } from '../interfaces/coursesList';
 import { fakeCourses } from '../fake-data';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss',
 })
 export class CoursesComponent {
   coursesList: CoursesList[] = [];
-  constructor() {
+  filteredCourses: CoursesList[] = [];
+  constructor() {}
+  ngOnInit(): void {
     this.coursesList = fakeCourses;
+    this.filteredCourses = fakeCourses;
   }
-  // ngOninit(): void {
-
-  // }
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredCourses = this.coursesList;
+      return;
+    }
+    this.filteredCourses = this.coursesList.filter((coursesList) =>
+      coursesList?.name.toLowerCase().includes(text.toLowerCase())
+    );
+  }
 }
