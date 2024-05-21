2c402d606795ff79dceae9443cef6a10d84f017b
khoi <hnak2442001smg4@gmail.com>
2024-05-21 21:34:47 +0700

    change search function, change table text, theme

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -3,14 +3,17 @@ import { CoursesList } from '../interfaces/coursesList';
 import { fakeCourses } from '../fake-data';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss',
 })
 export class CoursesComponent {
+  // finished: boolean = true;
+  // ongoing: boolean = true;
   coursesList: CoursesList[] = [];
   filteredCourses: CoursesList[] = [];
   constructor() {}
@@ -19,12 +22,15 @@ export class CoursesComponent {
     this.filteredCourses = fakeCourses;
   }
   filterResults(text: string) {
-    if (!text) {
-      this.filteredCourses = this.coursesList;
-      return;
-    }
-    this.filteredCourses = this.coursesList.filter((coursesList) =>
-      coursesList?.name.toLowerCase().includes(text.toLowerCase())
+    // if (!text) {
+    //   this.filteredCourses = this.coursesList;
+    //   return;
+    // }
+    this.filteredCourses = this.coursesList.filter(
+      (coursesList) =>
+        coursesList?.name.toLowerCase().includes(text.toLowerCase()) ||
+        coursesList?.lecturer.toLowerCase().includes(text.toLowerCase()) ||
+        coursesList?.semester.toLowerCase().includes(text.toLowerCase())
     );
   }
 }
